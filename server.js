acceabd0b5e79fb06f3dd5a1b6f43853ba43bd24
tmsheipline <taylorsheipline_2012@hotmail.com>
2022-05-31 15:50:21 -0400

    Greetings Finished

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/greeting/:name', (req,res) => {
-    res.send(`What's up `)
+    res.send(`What's up ${req.params.name}`)
 })
 
 app.listen(port, () => {
