51b2394ae0110a60309d40dc67c0f8891ded2a3b
tmsheipline <taylorsheipline_2012@hotmail.com>
2022-05-31 15:48:25 -0400

    Intial setup of express and app and port

--- server.js
+++ server.js
@@ -2,3 +2,11 @@ const express = require('express')
 const app = express();
 
 const port = 3000;
+
+app.get('/greeting/:name', (req,res) => {
+    res.send(`What's up `)
+})
+
+app.listen(port, () => {
+    console.log('app is running on port', port)
+});
