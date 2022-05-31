21f38dc497f10bfc94c1527e6cc27f5f2b484bf6
tmsheipline <taylorsheipline_2012@hotmail.com>
2022-05-31 16:18:53 -0400

    Tip Calculator express application created

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ app.get('/greeting/:name', (req,res) => {
     res.send(`What's up ${req.params.name}`)
 })
 
+
+app.get('/tip/:total/:tipPercentage', (req,res) => {
+    let total = Number(req.params.total);
+    let tipPercentage = Number(req.params.tipPercentage);
+    let tip = (tipPercentage / 100) * total
+    // let sum = total(tipPercentage / 100)
+    res.send(`${tip}`)  
+})
+
+
+
+
+
 app.listen(port, () => {
     console.log('app is running on port', port)
 });
